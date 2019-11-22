3864c40fb8bf5a08f8ec3b9c57c2d7d6753623dd
rxx78 <studior2road@gmail.com>
2019-11-22 22:12:44 +0900

    cleanup

--- cocos2dx_playground/Classes/RootScene.cpp
+++ cocos2dx_playground/Classes/RootScene.cpp
@@ -21,62 +21,22 @@ Scene* RootScene::create()
 	return ret;
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
-
-// on "init" you need to initialize your instance
 bool RootScene::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
-    {
+	if ( !Scene::init() )
         return false;
-    }
 
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 
-    /////////////////////////////
-    // 3. add your codes below...
+	auto label = Label::createWithTTF("[ESC] : Exit", "fonts/Marker Felt.ttf", 24);
+	label->setPosition(Vec2(
+		origin.x + visibleSize.width/2
+		, origin.y + visibleSize.height - label->getContentSize().height
+	));
+	addChild(label, 1);
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("[ESC] : Exit", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
     return true;
 }
 
