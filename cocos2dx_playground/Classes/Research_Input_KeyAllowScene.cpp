b36f82228fb5c8b57a031b31bbb939dfd31e0d71
rxx78 <studior2road@gmail.com>
2019-12-15 18:19:07 +0900

    extract

--- cocos2dx_playground/Classes/Research_Input_KeyAllowScene.cpp
+++ cocos2dx_playground/Classes/Research_Input_KeyAllowScene.cpp
@@ -16,6 +16,35 @@ namespace Research
 		namespace
 		{
 			const int TAG_KeyAllowControl_BG = 20140416;
+
+			Node* createKeyAllowControl( const ui::Widget::ccWidgetTouchCallback& _callback )
+			{
+				auto key_allow_control_root = Node::create();
+				{
+					const Size key_allow_margin( 8.f, 4.f );
+
+					auto key_allow_label = Label::createWithTTF( "RIGHT_PARENTHESIS", "fonts/arial.ttf", 10, Size::ZERO, TextHAlignment::CENTER );
+					key_allow_label->getFontAtlas()->setAliasTexParameters();
+					key_allow_control_root->addChild( key_allow_label, 2 );
+
+					auto button = ui::Button::create( "textures/ui/guide_01_1.png", "textures/ui/guide_01_2.png", "textures/ui/guide_01_1.png", ui::Widget::TextureResType::LOCAL );
+					button->getRendererNormal()->getTexture()->setAliasTexParameters();
+					button->getRendererClicked()->getTexture()->setAliasTexParameters();
+					button->getRendererDisabled()->getTexture()->setAliasTexParameters();
+					button->setScale9Enabled( true );
+					button->setContentSize( key_allow_label->getContentSize() + ( key_allow_margin * 2 ) );
+					button->addTouchEventListener( _callback );
+					key_allow_control_root->addChild( button, 1 );
+
+					auto bg = ui::Scale9Sprite::create( "textures/ui/guide_01_3.png" );
+					bg->setTag( TAG_KeyAllowControl_BG );
+					bg->setVisible( false );
+					bg->setContentSize( button->getContentSize() );
+					key_allow_control_root->addChild( bg, 0 );
+				}
+
+				return key_allow_control_root;
+			}
 		}
 
 		KeyAllowScene::KeyAllowScene() :
@@ -65,34 +94,12 @@ namespace Research
 			allowed_keys = CPG::Input::AllowedKeys::load( "research_input_allowedKeysTest_allowed_keys.json" );
 
 
-			auto key_allow_control_root = Node::create();
+			auto key_allow_control_root = createKeyAllowControl( CC_CALLBACK_2( KeyAllowScene::onKeyAllowControl, this ) );
 			key_allow_control_root->setPosition( Vec2(
 				origin.x + ( visibleSize.width * 0.5f )
 				, origin.y + ( visibleSize.height * 0.5f )
 			) );
 			addChild( key_allow_control_root, 1 );
-			{
-				const Size key_allow_margin( 8.f, 4.f );
-
-				auto key_allow_label = Label::createWithTTF( "RIGHT_PARENTHESIS", "fonts/arial.ttf", 10, Size::ZERO, TextHAlignment::CENTER );
-				key_allow_label->getFontAtlas()->setAliasTexParameters();
-				key_allow_control_root->addChild( key_allow_label, 2 );
-
-				auto button = ui::Button::create( "textures/ui/guide_01_1.png", "textures/ui/guide_01_2.png", "textures/ui/guide_01_1.png", ui::Widget::TextureResType::LOCAL );
-				button->getRendererNormal()->getTexture()->setAliasTexParameters();
-				button->getRendererClicked()->getTexture()->setAliasTexParameters();
-				button->getRendererDisabled()->getTexture()->setAliasTexParameters();
-				button->setScale9Enabled( true );
-				button->setContentSize( key_allow_label->getContentSize() + ( key_allow_margin * 2 ) );
-				button->addTouchEventListener( CC_CALLBACK_2( KeyAllowScene::onKeyAllowControl, this ) );
-				key_allow_control_root->addChild( button, 1 );
-
-				auto bg = ui::Scale9Sprite::create( "textures/ui/guide_01_3.png" );
-				bg->setTag( TAG_KeyAllowControl_BG );
-				bg->setVisible( false );
-				bg->setContentSize( button->getContentSize() );
-				key_allow_control_root->addChild( bg, 0 );
-			}
 
 			return true;
 		}
