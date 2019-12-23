8ce4d34e30c0311827f99fe6bc6625d0da9de3a4
rxx78 <studior2road@gmail.com>
2019-12-23 19:57:36 +0900

    add key map config control

--- cocos2dx_playground/Classes/Research_Input_KeyConfigScene.cpp
+++ cocos2dx_playground/Classes/Research_Input_KeyConfigScene.cpp
@@ -7,12 +7,48 @@
 #include "CPG_Input_BasicCollector.h"
 #include "CPG_InputKeyMap.h"
 
+#include "ui/UIButton.h"
+#include "ui/UIScale9Sprite.h"
+
 USING_NS_CC;
 
 namespace Research
 {
 	namespace Input
 	{
+		namespace
+		{
+			const int TAG_KeyConfigControl_BG = 20140416;
+
+			Node* createKeyConfigControl( const Size _control_size, const std::string& _key_name, const int _key_idx, const ui::Widget::ccWidgetTouchCallback& _callback )
+			{
+				auto root = Node::create();
+				root->setContentSize( _control_size );
+				{
+					auto label = Label::createWithTTF( _key_name, "fonts/arial.ttf", 10, Size::ZERO, TextHAlignment::CENTER );
+					root->addChild( label, 2 );
+
+					auto button = ui::Button::create( "textures/ui/guide_01_1.png", "textures/ui/guide_01_2.png", "textures/ui/guide_01_1.png", ui::Widget::TextureResType::LOCAL );
+					button->setTag( static_cast<int>( _key_idx ) );
+					button->getRendererNormal()->getTexture()->setAliasTexParameters();
+					button->getRendererClicked()->getTexture()->setAliasTexParameters();
+					button->getRendererDisabled()->getTexture()->setAliasTexParameters();
+					button->setScale9Enabled( true );
+					button->setContentSize( _control_size );
+					button->addTouchEventListener( _callback );
+					root->addChild( button, 1 );
+
+					auto indicator = ui::Scale9Sprite::create( "textures/ui/guide_01_3.png" );
+					indicator->setTag( TAG_KeyConfigControl_BG );
+					indicator->setVisible( false );
+					indicator->setContentSize( _control_size );
+					root->addChild( indicator, 0 );
+				}
+
+				return root;
+			}
+		}
+
 		Scene* KeyConfigScene::create()
 		{
 			auto ret = new ( std::nothrow ) KeyConfigScene();
@@ -68,8 +104,18 @@ namespace Research
 			{
 				ret->keymap_config_helper.load( "research_input_keyconfigscene_keymap.json" );
 
+				int count = 0;
 				for( const auto& h : ret->keymap_config_helper.getContainer() )
-					CCLOG( "%s", h.name );
+				{
+					auto control = createKeyConfigControl( Size( 60, 20 ), h.name, h.idx, CC_CALLBACK_2( KeyConfigScene::onKeyConfigControl, ret ) );
+					control->setPosition( Vec2(
+						origin.x + ( visibleSize.width * 0.5f )
+						, origin.y + ( control->getContentSize().height * 0.5f ) + 4.f + ( control->getContentSize().height * count )
+					) );
+					ret->addChild( control );
+
+					++count;
+				}
 			}
 
 
@@ -85,6 +131,16 @@ namespace Research
 			{
 				Director::getInstance()->replaceScene( RootScene::create() );
 			}
+		}
+
+		void KeyConfigScene::onKeyConfigControl( Ref* _sender, ui::Widget::TouchEventType _touch_event_type )
+		{
+			if( ui::Widget::TouchEventType::ENDED != _touch_event_type )
+				return;
+
+			auto button = static_cast<ui::Button*>( _sender );
+			auto bg = button->getParent()->getChildByTag( TAG_KeyConfigControl_BG );
+			bg->setVisible( !bg->isVisible() );
 		}
 	}
 }
--- cocos2dx_playground/Classes/Research_Input_KeyConfigScene.h
+++ cocos2dx_playground/Classes/Research_Input_KeyConfigScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "ui/UIWidget.h"
 
 #include "CPG_Input_KeyMapConfigHelper.h"
 
@@ -25,6 +26,9 @@ namespace Research
 			static cocos2d::Scene* create();
 
 			void update( float dt ) override;
+
+		private:
+			void onKeyConfigControl( cocos2d::Ref* _sender, cocos2d::ui::Widget::TouchEventType _touch_event_type );
 
 		private:
 			CPG::Input::KeyCollectorSp input_collector;
