1025743fc8640603c94e3a00c9ae779516077327
rxx78 <studior2road@gmail.com>
2020-01-10 20:49:00 +0900

    remove warning

--- cocos2dx_playground/proj.win32/main.cpp
+++ cocos2dx_playground/proj.win32/main.cpp
@@ -32,10 +32,10 @@
 
 USING_NS_CC;
 
-int WINAPI _tWinMain(HINSTANCE hInstance,
+int WINAPI _tWinMain(HINSTANCE /*hInstance*/,
                        HINSTANCE hPrevInstance,
                        LPTSTR    lpCmdLine,
-                       int       nCmdShow)
+                       int       /*nCmdShow*/ )
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
