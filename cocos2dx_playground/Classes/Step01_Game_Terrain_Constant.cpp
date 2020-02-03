e7b72c4542e108768b05dadec66c69f8300b330c
r2road <studior2road@gmail.com>
2020-02-04 00:05:22 +0900

    cleanup

--- cocos2dx_playground/Classes/Step01_Game_Terrain_Constant.cpp
+++ cocos2dx_playground/Classes/Step01_Game_Terrain_Constant.cpp
@@ -1,46 +1,35 @@
 #include "Step01_Game_Terrain_Constant.h"
 
+#include <array>
+
 namespace step01
 {
 	namespace game
 	{
 		namespace terrain
 		{
+			struct TileData
+			{
+				eTileType TileType = eTileType::damage;
+				char* ResourcePath = "";
+			};
+			const std::array<TileData, static_cast<std::size_t>( eTileType::SIZE )> TileTable = { {
+				{ eTileType::damage, "step01_game_tile_damage.png" }
+				,{ eTileType::road, "step01_game_tile_road.png" }
+				,{ eTileType::entrance, "step01_game_tile_entrance.png" }
+				,{ eTileType::exit, "step01_game_tile_exit.png" }
+				,{ eTileType::magic_circle_on, "step01_game_tile_magic_circle_01.png" }
+				,{ eTileType::magic_circle_off, "step01_game_tile_magic_circle_02.png" }
+			} };
+
 			const char* TileType2TilePath( const eTileType tile_type )
 			{
-				static const char* damage_tile_path = "step01_game_tile_damage.png";
-				static const char* road_tile_path = "step01_game_tile_road.png";
-				static const char* entrance_tile_path = "step01_game_tile_entrance.png";
-				static const char* exit_tile_path = "step01_game_tile_exit.png";
-				static const char* magic_ciecle_on_tile_path = "step01_game_tile_magic_circle_01.png";
-				static const char* magic_ciecle_off_tile_path = "step01_game_tile_magic_circle_02.png";
-
-				if( eTileType::damage == tile_type )
+				if( eTileType::FIRST > tile_type || eTileType::SIZE <= tile_type )
 				{
-					return damage_tile_path;
-				}
-				else if( eTileType::road == tile_type )
-				{
-					return road_tile_path;
-				}
-				else if( eTileType::entrance == tile_type )
-				{
-					return entrance_tile_path;
-				}
-				else if( eTileType::exit == tile_type )
-				{
-					return exit_tile_path;
-				}
-				else if( eTileType::magic_circle_on == tile_type )
-				{
-					return magic_ciecle_on_tile_path;
-				}
-				else if( eTileType::magic_circle_off == tile_type )
-				{
-					return magic_ciecle_off_tile_path;
+					return TileTable[0u].ResourcePath;
 				}
 
-				return damage_tile_path;
+				return TileTable[static_cast<std::size_t>( tile_type )].ResourcePath;
 			}
 		}
 	}
