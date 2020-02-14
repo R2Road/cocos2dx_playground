4d03b0ba4ac283b22b3cd4fce2b4032c0ddfa450
r2road <studior2road@gmail.com>
2020-02-15 00:31:09 +0900

    cleanup

--- cocos2dx_playground/Classes/AppDelegate.cpp
+++ cocos2dx_playground/Classes/AppDelegate.cpp
@@ -23,22 +23,13 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "EntryScene.h"
-
-#include "CPG_Setting.h"
 
 #include "audio/include/AudioEngine.h"
 
+#include "EntryScene.h"
+#include "CPG_Setting.h"
+
 USING_NS_CC;
-
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
-
-AppDelegate::AppDelegate()
-{
-}
 
 AppDelegate::~AppDelegate() 
 {
--- cocos2dx_playground/Classes/AppDelegate.h
+++ cocos2dx_playground/Classes/AppDelegate.h
@@ -35,7 +35,7 @@ Private inheritance here hides part of interface from Director.
 class  AppDelegate : private cocos2d::Application
 {
 public:
-    AppDelegate();
+    AppDelegate() = default;
     virtual ~AppDelegate();
 
     virtual void initGLContextAttrs();
