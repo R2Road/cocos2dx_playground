aed57c4a4eb87a155c72d271cb06d8ca99eb5bf5
r2road <studior2road@gmail.com>
2020-02-25 11:33:27 +0900

    update component : cpg - Collision - add radius viewer

--- cocos2dx_playground/Classes/cpg_CollisionComponent.cpp
+++ cocos2dx_playground/Classes/cpg_CollisionComponent.cpp
@@ -1,14 +1,21 @@
 #include "cpg_CollisionComponent.h"
 
 #include <new>
+#include <numeric>
+
+#include "cocos/base/ccUTF8.h"
 
 USING_NS_CC;
 
 namespace cpg
 {
-	CollisionComponent::CollisionComponent( const float radius ) : mRadius( radius )
+	CollisionComponent::CollisionComponent( const float radius ) : mRadius( radius ), mLabel( nullptr )
 	{
 		setName( "CPG_COLLISION" );
+	}
+	CollisionComponent::~CollisionComponent()
+	{
+		mLabel->release();
 	}
 
 	CollisionComponent* CollisionComponent::create( const float radius )
@@ -26,5 +33,29 @@ namespace cpg
 		}
 
 		return ret;
+	}
+
+	bool CollisionComponent::init()
+	{
+		if( !ParentT::init() )
+		{
+			return false;
+		}
+
+		mLabel = Label::createWithTTF( StringUtils::format( "%.2f", mRadius ), "fonts/arial.ttf", 9, Size::ZERO, TextHAlignment::LEFT );
+		mLabel->setAnchorPoint( Vec2( 0.f, 0.5f ) );
+		mLabel->setPositionX( mRadius );
+		mLabel->retain();
+
+		return true;
+	}
+
+	void CollisionComponent::onAdd()
+	{
+		_owner->addChild( mLabel, std::numeric_limits<int>::max() );
+	}
+	void CollisionComponent::onRemove()
+	{
+		_owner->removeChild( mLabel );
 	}
 }
--- cocos2dx_playground/Classes/cpg_CollisionComponent.h
+++ cocos2dx_playground/Classes/cpg_CollisionComponent.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include "cocos/2d/CCComponent.h"
+#include "cocos/2d/CCLabel.h"
+#include "cocos/base/CCRefPtr.h"
 
 namespace cpg
 {
 	class CollisionComponent : public cocos2d::Component
 	{
 	private:
+		using ParentT = cocos2d::Component;
+
 		CollisionComponent( const float radius );
+		~CollisionComponent();
 
 	public:
 		static CollisionComponent* create( const float radius );
@@ -15,6 +20,12 @@ namespace cpg
 		float GetRadius() const { return mRadius; }
 
 	private:
+		bool init() override;
+		void onAdd() override;
+		void onRemove() override;
+
+	private:
 		const float mRadius;
+		cocos2d::RefPtr<cocos2d::Label> mLabel;
 	};
 }
