05775477d515556aab941f9e6b257df32a1b3b94
r2road <studior2road@gmail.com>
2020-03-03 08:46:27 +0900

    add scene

---
+++ cocos2dx_playground/Classes/step02_fsm1test_BasicScene.cpp
@@ -0,0 +1,105 @@
+#include "step02_fsm1test_BasicScene.h"
+
+#include <new>
+#include <sstream>
+
+#include "step02_RootScene.h"
+
+USING_NS_CC;
+
+namespace step02
+{
+	namespace fsm1test
+	{
+		BasicScene::BasicScene() : mKeyboardListener( nullptr )
+		{}
+
+		Scene* BasicScene::create()
+		{
+			auto ret = new ( std::nothrow ) BasicScene();
+			if( !ret || !ret->init() )
+			{
+				delete ret;
+				ret = nullptr;
+				return nullptr;
+			}
+			else
+			{
+				ret->autorelease();
+			}
+
+			return ret;
+		}
+
+		bool BasicScene::init()
+		{
+			if( !Scene::init() )
+			{
+				return false;
+			}
+
+			const auto visibleSize = Director::getInstance()->getVisibleSize();
+			const auto visibleOrigin = Director::getInstance()->getVisibleOrigin();
+
+			//
+			// Summury
+			//
+			{
+				std::stringstream ss;
+				ss << "+ " << getTitle();
+				ss << "\n";
+				ss << "\n";
+				ss << "[ESC] : Return to Root";
+
+				auto label = Label::createWithTTF( ss.str(), "fonts/arial.ttf", 9, Size::ZERO, TextHAlignment::LEFT );
+				label->setAnchorPoint( Vec2( 0.f, 1.f ) );
+				label->setPosition( Vec2(
+					visibleOrigin.x
+					, visibleOrigin.y + visibleSize.height
+				) );
+				addChild( label, 9999 );
+			}
+			
+			//
+			// Background
+			//
+			{
+				auto background_layer = LayerColor::create( Color4B( 5, 29, 81, 255 ) );
+				addChild( background_layer, 0 );
+			}
+
+			return true;
+		}
+
+		void BasicScene::onEnter()
+		{
+			Scene::onEnter();
+			mKeyboardListener = EventListenerKeyboard::create();
+			mKeyboardListener->onKeyPressed = CC_CALLBACK_2( BasicScene::onKeyPressed, this );
+			getEventDispatcher()->addEventListenerWithFixedPriority( mKeyboardListener, 1 );
+		}
+		void BasicScene::onExit()
+		{
+			assert( mKeyboardListener );
+			getEventDispatcher()->removeEventListener( mKeyboardListener );
+			mKeyboardListener = nullptr;
+			Node::onExit();
+		}
+
+		void BasicScene::updateForExit( float /*dt*/ )
+		{
+			Director::getInstance()->replaceScene( step02::RootScene::create() );
+		}
+
+		void BasicScene::onKeyPressed( EventKeyboard::KeyCode keycode, Event* /*event*/ )
+		{
+			if( EventKeyboard::KeyCode::KEY_ESCAPE == keycode )
+			{
+				if( !isScheduled( schedule_selector( BasicScene::updateForExit ) ) )
+				{
+					scheduleOnce( schedule_selector( BasicScene::updateForExit ), 0.f );
+				}
+			}
+		}
+	}
+}
---
+++ cocos2dx_playground/Classes/step02_fsm1test_BasicScene.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#include "2d/CCScene.h"
+
+namespace step02
+{
+	namespace fsm1test
+	{
+		class BasicScene : public cocos2d::Scene
+		{
+		private:
+			BasicScene();
+
+		public:
+			static const char* getTitle() { return "FSM1 Test : Basic"; }
+			static cocos2d::Scene* create();
+
+			bool init() override;
+			void onEnter() override;
+			void onExit() override;
+
+		private:
+			void updateForExit( float dt );
+			void onKeyPressed( cocos2d::EventKeyboard::KeyCode keycode, cocos2d::Event* /*event*/ );
+
+		private:
+			cocos2d::EventListenerKeyboard* mKeyboardListener;
+		};
+	}
+}
