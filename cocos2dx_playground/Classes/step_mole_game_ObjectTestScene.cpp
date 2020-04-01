2134f3fc0138f0cf5f47e9a94783cb055058b669
r2road <studior2road@gmail.com>
2020-04-02 05:40:17 +0900

    summury

--- cocos2dx_playground/Classes/step_mole_game_ObjectTestScene.cpp
+++ cocos2dx_playground/Classes/step_mole_game_ObjectTestScene.cpp
@@ -72,9 +72,9 @@ namespace step_mole
 				ss << "[ESC] : Return to Root";
 				ss << std::endl;
 				ss << std::endl;
-				ss << "[1] : Start Action";
+				ss << "[1] : Process Start( visible-on > ani > ani > ani > visible-off )";
 				ss << std::endl;
-				ss << "[2] : End Action";
+				ss << "[2] : Process End( ani > visible-off ) : Possible when Actor is Visible";
 
 				auto label = Label::createWithTTF( ss.str(), "fonts/arial.ttf", 9, Size::ZERO, TextHAlignment::LEFT );
 				label->setAnchorPoint( Vec2( 0.f, 1.f ) );
