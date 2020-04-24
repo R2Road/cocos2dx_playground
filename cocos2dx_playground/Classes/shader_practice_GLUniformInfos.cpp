cd100fd9c82c498faa8727dd737783ce3a09a16d
r2road <studior2road@gmail.com>
2020-04-24 19:20:41 +0900

    update class : GLUniformInfos

--- cocos2dx_playground/Classes/shader_practice_GLUniformInfos.cpp
+++ cocos2dx_playground/Classes/shader_practice_GLUniformInfos.cpp
@@ -17,57 +17,59 @@ namespace shader_practice
 		mContainer.clear();
 
 		// Query and store uniforms from the program.
-		GLint activeUniforms;
-		glGetProgramiv( program_index, GL_ACTIVE_UNIFORMS, &activeUniforms );
-		if( activeUniforms > 0 )
-		{
-			GLint length;
-			glGetProgramiv( program_index, GL_ACTIVE_UNIFORM_MAX_LENGTH, &length );
-			if( length > 0 )
-			{
-				GLint uniform_size; /**Number of data type in attribute.*/
-				GLenum uniform_type; /**Data type of the attribute.*/
-				GLchar* uniform_name = (GLchar*)alloca( length + 1 );
-
-				for( int i = 0; i < activeUniforms; ++i )
-				{
-					// Query uniform info.
-					glGetActiveUniform( program_index, i, length, nullptr, &uniform_size, &uniform_type, uniform_name );
-					uniform_name[length] = '\0';
-
-					CCLOG( "Checked Name : %s, %d", uniform_name, uniform_type );
-
-					// Only add uniforms that are not built-in.
-					// The ones that start with 'CC_' are built-ins
-					if( strncmp( "CC_", uniform_name, 3 ) != 0 )
-					{
-						// remove possible array '[]' from uniform name
-						if( length > 3 )
-						{
-							char* c = strrchr( uniform_name, '[' );
-							if( c )
-							{
-								*c = '\0';
-							}
-						}
-
-						GLenum __gl_error_code = glGetError();
-						if( __gl_error_code != GL_NO_ERROR )
-						{
-							CCLOG( "error: 0x%x  uniformName: %s", (int)__gl_error_code, uniform_name );
-						}
-						assert( __gl_error_code == GL_NO_ERROR );
-
-						mContainer.emplace_back( uniform_name, uniform_type );
-					}
-				}
-			}
-		}
-		else
+		GLint active_uniform_count;
+		glGetProgramiv( program_index, GL_ACTIVE_UNIFORMS, &active_uniform_count );
+		if( active_uniform_count <= 0 )
 		{
 			GLchar ErrorLog[1024];
 			glGetProgramInfoLog( program_index, sizeof( ErrorLog ), nullptr, ErrorLog );
 			CCLOG( "Error linking shader program: '%s'\n", ErrorLog );
+			return;
+		}
+
+		GLint active_uniform_max_length;
+		glGetProgramiv( program_index, GL_ACTIVE_UNIFORM_MAX_LENGTH, &active_uniform_max_length );
+		if( active_uniform_max_length <= 0 )
+		{
+			return;
+		}
+
+		GLint uniform_size; /**Number of data type in attribute.*/
+		GLenum uniform_type; /**Data type of the attribute.*/
+		GLchar* uniform_name = (GLchar*)alloca( active_uniform_max_length + 1 );
+		for( int i = 0; i < active_uniform_count; ++i )
+		{
+			// Query uniform info.
+			glGetActiveUniform( program_index, i, active_uniform_max_length, nullptr, &uniform_size, &uniform_type, uniform_name );
+			uniform_name[active_uniform_max_length] = '\0';
+
+			CCLOG( "Checked Name : %s, %d", uniform_name, uniform_type );
+
+			// Only add uniforms that are not built-in.
+			// The ones that start with 'CC_' are built-ins
+			if( strncmp( "CC_", uniform_name, 3 ) == 0 )
+			{
+				continue;
+			}
+
+			// remove possible array '[]' from uniform name : ex) u_Color[0]
+			if( active_uniform_max_length > 3 )
+			{
+				char* c = strrchr( uniform_name, '[' );
+				if( c )
+				{
+					*c = '\0';
+				}
+			}
+
+			GLenum __gl_error_code = glGetError();
+			if( __gl_error_code != GL_NO_ERROR )
+			{
+				CCLOG( "error: 0x%x  uniformName: %s", (int)__gl_error_code, uniform_name );
+			}
+			assert( __gl_error_code == GL_NO_ERROR );
+
+			mContainer.emplace_back( uniform_name, uniform_type );
 		}
 	}
 
