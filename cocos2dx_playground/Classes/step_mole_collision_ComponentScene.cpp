b1877c880c51c0b2498f2ac5e9639964c0832dad
r2road <studior2road@gmail.com>
2020-06-10 06:08:36 +0900

    cleanup

--- cocos2dx_playground/Classes/step_mole_collision_ComponentScene.cpp
+++ cocos2dx_playground/Classes/step_mole_collision_ComponentScene.cpp
@@ -93,7 +93,7 @@ namespace step_mole
 			//
 			{
 				auto background_layer = LayerColor::create( Color4B( 15, 49, 101, 255 ) );
-				addChild( background_layer, -1 );
+				addChild( background_layer, std::numeric_limits<int>::min() );
 			}
 
 			//
