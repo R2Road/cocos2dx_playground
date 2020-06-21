41e04cab4577a69c34fcc225d5bb380c8a30d933
r2road <studior2road@gmail.com>
2020-06-22 03:24:51 +0900

    change title

--- cocos2dx_playground/Classes/step_rain_of_chaos_RootScene.h
+++ cocos2dx_playground/Classes/step_rain_of_chaos_RootScene.h
@@ -9,7 +9,7 @@ namespace step_rain_of_chaos
 	public:
 		RootScene();
 
-		static const char* getTitle() { return "Step : Rain of Chaos( To Do )"; }
+		static const char* getTitle() { return "Step : Rain of Chaos( In Progress )"; }
 		static cocos2d::Scene* create();
 
 		void onEnter() override;
