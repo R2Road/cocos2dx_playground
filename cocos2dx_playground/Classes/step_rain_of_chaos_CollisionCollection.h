42bf0137050be70374162f594e53a653a51d0696
r2road <studior2road@gmail.com>
2020-07-03 05:50:55 +0900

    using

--- cocos2dx_playground/Classes/step_rain_of_chaos_CollisionCollection.h
+++ cocos2dx_playground/Classes/step_rain_of_chaos_CollisionCollection.h
@@ -12,11 +12,13 @@ namespace step_rain_of_chaos
 	class CollisionCollection
 	{
 	public:
+		using ContainerT = std::list<step_mole::CircleCollisionComponent*>;
+
 		void Add( step_mole::CircleCollisionComponent* component );
 		void Remove( step_mole::CircleCollisionComponent* component );
 		void Clear() { mCollisionList.clear(); }
 
 	private:
-		std::list<step_mole::CircleCollisionComponent*> mCollisionList;
+		ContainerT mCollisionList;
 	};
 }
