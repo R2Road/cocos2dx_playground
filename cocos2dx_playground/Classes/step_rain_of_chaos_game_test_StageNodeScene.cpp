e1507e4ca006a4610abdce03f83c2d7c5f5cdb03
r2road <studior2road@gmail.com>
2020-07-08 10:42:52 +0900

    add scene : step_rain_of_chaos - game_test - StageNodeScene

---
+++ cocos2dx_playground/Classes/step_rain_of_chaos_game_test_StageNodeScene.cpp
@@ -0,0 +1,112 @@
+#include "step_rain_of_chaos_game_test_StageNodeScene.h"
+
+#include <new>
+#include <numeric>
+#include <sstream>
+
+#include "2d/CCLabel.h"
+#include "2d/CCLayer.h"
+#include "base/CCDirector.h"
+#include "base/CCEventListenerKeyboard.h"
+#include "base/CCEventDispatcher.h"
+
+USING_NS_CC;
+
+namespace step_rain_of_chaos
+{
+	namespace game_test
+	{
+		StageNodeScene::StageNodeScene( const helper::FuncSceneMover& back_to_the_previous_scene_callback ) :
+			helper::BackToThePreviousScene( back_to_the_previous_scene_callback )
+			, mKeyboardListener( nullptr )
+		{}
+
+		Scene* StageNodeScene::create( const helper::FuncSceneMover& back_to_the_previous_scene_callback )
+		{
+			auto ret = new ( std::nothrow ) StageNodeScene( back_to_the_previous_scene_callback );
+			if( !ret || !ret->init() )
+			{
+				delete ret;
+				ret = nullptr;
+				return nullptr;
+			}
+			else
+			{
+				ret->autorelease();
+			}
+
+			return ret;
+		}
+
+		bool StageNodeScene::init()
+		{
+			if( !Scene::init() )
+			{
+				return false;
+			}
+
+			const auto visibleSize = Director::getInstance()->getVisibleSize();
+			const auto visibleOrigin = Director::getInstance()->getVisibleOrigin();
+
+			//
+			// Summury
+			//
+			{
+				std::stringstream ss;
+				ss << "+ " << getTitle();
+				ss << std::endl;
+				ss << std::endl;
+				ss << "[ESC] : Return to Root";
+
+				auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 10, Size::ZERO, TextHAlignment::LEFT );
+				label->setAnchorPoint( Vec2( 0.f, 1.f ) );
+				label->setPosition( Vec2(
+					visibleOrigin.x
+					, visibleOrigin.y + visibleSize.height
+				) );
+				addChild( label, std::numeric_limits<int>::max() );
+			}
+			
+			//
+			// Background
+			//
+			{
+				auto background_layer = LayerColor::create( Color4B( 3, 20, 70, 255 ) );
+				addChild( background_layer, std::numeric_limits<int>::min() );
+			}
+
+			return true;
+		}
+
+		void StageNodeScene::onEnter()
+		{
+			Scene::onEnter();
+
+			assert( !mKeyboardListener );
+			mKeyboardListener = EventListenerKeyboard::create();
+			mKeyboardListener->onKeyPressed = CC_CALLBACK_2( StageNodeScene::onKeyPressed, this );
+			getEventDispatcher()->addEventListenerWithSceneGraphPriority( mKeyboardListener, this );
+		}
+		void StageNodeScene::onExit()
+		{
+			assert( mKeyboardListener );
+			getEventDispatcher()->removeEventListener( mKeyboardListener );
+			mKeyboardListener = nullptr;
+
+			Scene::onExit();
+		}
+
+		void StageNodeScene::onKeyPressed( EventKeyboard::KeyCode keycode, Event* /*event*/ )
+		{
+			switch( keycode )
+			{
+			case EventKeyboard::KeyCode::KEY_ESCAPE:
+				helper::BackToThePreviousScene::MoveBack();
+				return;
+
+			default:
+				CCLOG( "Key Code : %d", keycode );
+			}
+		}
+	}
+}
