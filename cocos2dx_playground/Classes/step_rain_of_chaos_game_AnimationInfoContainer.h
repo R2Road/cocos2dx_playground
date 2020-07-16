040199432cfd62ebff98304827cd6fdbac6b5608
r2road <studior2road@gmail.com>
2020-07-16 12:43:15 +0900

    add animation info container

---
+++ cocos2dx_playground/Classes/step_rain_of_chaos_game_AnimationInfoContainer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "cpg_animation_InfoContainer.h"
+
+namespace step_rain_of_chaos
+{
+	namespace game
+	{
+		const cpg::animation::InfoContainerT& GetBulletAnimationInfoContainer();
+	}
+}
