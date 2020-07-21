9ed84e80d938d0cc7f655b42bc904b3ca1bf2818
r2road <studior2road@gmail.com>
2020-07-22 03:21:04 +0900

    frame test : safety count of bullet is 1000 - in debug mode

--- cocos2dx_playground/Classes/step_rain_of_chaos_game_test_StageNodeScene.cpp
+++ cocos2dx_playground/Classes/step_rain_of_chaos_game_test_StageNodeScene.cpp
@@ -159,7 +159,7 @@ namespace step_rain_of_chaos
 					mStageConfig
 					, game::StageNode::DebugConfig{ true, true }
 					, mBulletManager->GetComeHomeCallback()
-					, step_mole::CircleCollisionComponentConfig { true, true, true }
+					, step_mole::CircleCollisionComponentConfig { false, false, false }
 					, BulletCachingAmount
 				);
 				addChild( mStageNode );
@@ -229,7 +229,7 @@ namespace step_rain_of_chaos
 					dir.scale( mCurrentMoveSpeed );
 					mStageNode->RequestAction( target_index, Vec2( mStageConfig.GetStageArea().origin ) + offset, dir );
 
-					offset.y += 10.f;
+					offset.y += 0.5;
 				}
 
 				return;
