17880dab8aa8ebea5f3db959ea4e328a4415940c
r2road <studior2road@gmail.com>
2020-08-31 07:59:59 +0900

    ...

--- cocos2dx_playground/Classes/step_rain_of_chaos_game_test_EnemyProcessorScene.cpp
+++ cocos2dx_playground/Classes/step_rain_of_chaos_game_test_EnemyProcessorScene.cpp
@@ -89,7 +89,7 @@ namespace step_rain_of_chaos
 				ss << std::endl;
 				ss << "[SPACE] : Start Process";
 
-				auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 10, Size::ZERO, TextHAlignment::LEFT );
+				auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 8, Size::ZERO, TextHAlignment::LEFT );
 				label->setAnchorPoint( Vec2( 0.f, 1.f ) );
 				label->setPosition( Vec2(
 					visibleOrigin.x
