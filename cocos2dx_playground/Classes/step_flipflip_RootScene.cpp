c045566c267af43670f38e93f4f0582b72efcd52
r2road <studior2road@gmail.com>
2020-11-25 21:24:24 +0900

    add scene : step_flipflip - RootScene

---
+++ cocos2dx_playground/Classes/step_flipflip_RootScene.cpp
@@ -0,0 +1,107 @@
+#include "step_flipflip_RootScene.h"
+
+#include <new>
+#include <numeric>
+#include <sstream>
+
+#include "2d/CCLabel.h"
+#include "2d/CCLayer.h"
+#include "base/CCDirector.h"
+#include "base/CCEventListenerKeyboard.h"
+#include "base/CCEventDispatcher.h"
+
+#include "PlayGroundScene.h"
+
+USING_NS_CC;
+
+namespace step_flipflip
+{
+	RootScene::RootScene() : mKeyboardListener( nullptr ) {}
+
+	Scene* RootScene::create()
+	{
+		auto ret = new ( std::nothrow ) RootScene();
+		if( !ret || !ret->init() )
+		{
+			delete ret;
+			ret = nullptr;
+		}
+		else
+		{
+			ret->autorelease();
+		}
+
+		return ret;
+	}
+
+	bool RootScene::init()
+	{
+		if( !Scene::init() )
+		{
+			return false;
+		}
+
+		const auto visibleSize = _director->getVisibleSize();
+		const auto visibleOrigin = _director->getVisibleOrigin();
+
+		//
+		// Summury
+		//
+		{
+			std::stringstream ss;
+			ss << "+ " << getTitle();
+			ss << std::endl;
+			ss << std::endl;
+			ss << "[ESC] : Return to Playground";
+
+			auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 10, Size::ZERO, TextHAlignment::LEFT );
+			label->setPosition( Vec2(
+				visibleOrigin.x + ( visibleSize.width * 0.5f )
+				, visibleOrigin.y + ( visibleSize.height * 0.5f )
+			) );
+			addChild( label );
+		}
+
+		//
+		// Background
+		//
+		{
+			auto background_layer = LayerColor::create( Color4B( 36, 120, 122, 255 ) );
+			addChild( background_layer, std::numeric_limits<int>::min() );
+		}
+
+		return true;
+	}
+
+	void RootScene::onEnter()
+	{
+		Scene::onEnter();
+
+		assert( !mKeyboardListener );
+		mKeyboardListener = EventListenerKeyboard::create();
+		mKeyboardListener->onKeyPressed = CC_CALLBACK_2( RootScene::onKeyPressed, this );
+		getEventDispatcher()->addEventListenerWithSceneGraphPriority( mKeyboardListener, this );
+	}
+	void RootScene::onExit()
+	{
+		assert( mKeyboardListener );
+		getEventDispatcher()->removeEventListener( mKeyboardListener );
+		mKeyboardListener = nullptr;
+
+		Scene::onExit();
+	}
+
+
+	void RootScene::onKeyPressed( EventKeyboard::KeyCode keycode, Event* /*event*/ )
+	{
+		switch( keycode )
+		{
+		case EventKeyboard::KeyCode::KEY_ESCAPE:
+			_director->replaceScene( PlayGroundScene::create() );
+			break;
+
+		default:
+			CCLOG( "Key Code : %d", keycode );
+		}
+	}
+} // namespace step01
