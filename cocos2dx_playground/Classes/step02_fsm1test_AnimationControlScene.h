13697d281f61ff39dc7a5487cd9e00d5b259b2d2
r2road <studior2road@gmail.com>
2020-12-03 20:29:52 +0900

    ...

--- cocos2dx_playground/Classes/step02_fsm1test_AnimationControlScene.h
+++ cocos2dx_playground/Classes/step02_fsm1test_AnimationControlScene.h
@@ -16,7 +16,7 @@ namespace step02
 			AnimationControlScene( const helper::FuncSceneMover& back_to_the_previous_scene_callback );
 
 		public:
-			static const char* getTitle() { return "FSM1 Test : Animation Control"; }
+			static const char* getTitle() { return "FSM1 Test : Animation Control( To Do )"; }
 			static cocos2d::Scene* create( const helper::FuncSceneMover& back_to_the_previous_scene_callback );
 
 			bool init() override;
