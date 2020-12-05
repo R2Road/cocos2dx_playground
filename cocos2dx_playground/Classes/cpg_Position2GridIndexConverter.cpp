3353cda129e3308ab07c16614e0fbe75b957131a
r2road <studior2road@gmail.com>
2020-12-06 00:30:58 +0900

    not allow 0

--- cocos2dx_playground/Classes/cpg_Position2GridIndexConverter.cpp
+++ cocos2dx_playground/Classes/cpg_Position2GridIndexConverter.cpp
@@ -2,12 +2,17 @@
 
 #include <algorithm>
 
+#include "base/ccMacros.h"
+
 namespace cpg
 {
 	Position2GridIndexConverter::Position2GridIndexConverter( const int block_width, const int block_height ) :
 		mBlockWidth( block_width )
 		, mBlockHeight( block_height )
-	{}
+	{
+		CCASSERT( 0 != mBlockWidth, "Not Allow 0" );
+		CCASSERT( 0 != mBlockHeight, "Not Allow 0" );
+	}
 
 
 	Position2GridIndexConverter::Point Position2GridIndexConverter::Position2Point( const float x, const float y ) const
