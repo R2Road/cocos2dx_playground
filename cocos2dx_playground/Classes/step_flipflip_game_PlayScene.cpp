0be78af4fd4614a604d2459b9170fc2f647c0847
r2road <studior2road@gmail.com>
2020-12-22 00:54:28 +0900

    update scene : step_flipflip - game - PlayScene

--- cocos2dx_playground/Classes/step_flipflip_game_PlayScene.cpp
+++ cocos2dx_playground/Classes/step_flipflip_game_PlayScene.cpp
@@ -10,6 +10,11 @@
 #include "base/CCEventListenerKeyboard.h"
 #include "base/CCEventDispatcher.h"
 
+#include "step_flipflip_game_CardSelectorNode.h"
+#include "step_flipflip_game_Constant.h"
+#include "step_flipflip_game_StageData.h"
+#include "step_flipflip_game_StageViewNode.h"
+
 #include "step_flipflip_game_TitleScene.h"
 
 USING_NS_CC;
@@ -18,7 +23,11 @@ namespace step_flipflip
 {
 	namespace game
 	{
-		PlayScene::PlayScene() : mKeyboardListener( nullptr ) {}
+		PlayScene::PlayScene() :
+			mKeyboardListener( nullptr )
+			, mCardSelectorNode( nullptr )
+			, mStageViewNode( nullptr )
+		{}
 
 		Scene* PlayScene::create()
 		{
@@ -45,6 +54,10 @@ namespace step_flipflip
 
 			const auto visibleSize = _director->getVisibleSize();
 			const auto visibleOrigin = _director->getVisibleOrigin();
+			const Vec2 visibleCenter(
+				visibleOrigin
+				+ Vec2( visibleSize.width * 0.5f, visibleSize.height * 0.5f )
+			);
 
 			//
 			// Summury
@@ -53,13 +66,44 @@ namespace step_flipflip
 				std::stringstream ss;
 				ss << "[ESC] : Return to Root";
 
-				auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 8 );
+				auto label = Label::createWithTTF( ss.str(), "fonts/NanumSquareR.ttf", 7 );
 				label->setAnchorPoint( Vec2( 0.f, 1.f ) );
 				label->setPosition( Vec2(
 					visibleOrigin.x
 					, visibleOrigin.y + visibleSize.height
 				) );
 				addChild( label, std::numeric_limits<int>::max() );
+			}
+
+			//
+			// Stage Setup
+			//
+			const game::StageConfig STAGE_CONFIG{ 6, 3, cocos2d::Size( 40.f, 54.f ) };
+			game::StageData stage_data;
+			stage_data.Reset( STAGE_CONFIG.Width, STAGE_CONFIG.Height );
+
+			//
+			// Stage View Node
+			//
+			{
+				mStageViewNode = game::StageViewNode::create( STAGE_CONFIG, stage_data );
+				mStageViewNode->setPosition(
+					visibleCenter
+					- Vec2( mStageViewNode->getContentSize().width * 0.5f, mStageViewNode->getContentSize().height * 0.5f )
+				);
+				addChild( mStageViewNode );
+			}
+
+			//
+			// Card Selector Node
+			//
+			{
+				mCardSelectorNode = game::CardSelectorNode::create( STAGE_CONFIG );
+				mCardSelectorNode->setPosition(
+					visibleCenter
+					- Vec2( mCardSelectorNode->getContentSize().width * 0.5f, mCardSelectorNode->getContentSize().height * 0.5f )
+				);
+				addChild( mCardSelectorNode, 1 );
 			}
 
 			return true;
