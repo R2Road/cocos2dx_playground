ea7230bf92de64e7574a00e3c2222893a9f9ac37
r2road <studior2road@gmail.com>
2020-12-31 09:10:51 +0900

    cleanup

--- cocos2dx_playground/Classes/step_clickclick_game_StageView.cpp
+++ cocos2dx_playground/Classes/step_clickclick_game_StageView.cpp
@@ -12,34 +12,34 @@
 
 USING_NS_CC;
 
+namespace
+{
+#if defined( DEBUG ) || defined( _DEBUG )
+	#define CHECK_ODD_NUMBER( number ) ( assert( 1 == ( ( number ) & 1 ) ) )
+#else
+	#define CHECK_ODD_NUMBER( number )
+#endif
+}
+
 namespace step_clickclick
 {
 	namespace game
 	{
-		namespace
-		{
-#if defined( DEBUG ) || defined( _DEBUG )
-		#define CHECK_ODD_NUMBER( number ) ( assert( 1 == ( ( number ) & 1 ) ) )
-#else
-		#define CHECK_ODD_NUMBER( number )
-#endif
-		}
-
 		StageView::StageView( const int width, const int height ) :
 			mStageWidth( width )
 			, mStageHeight( height )
 			, mGridIndexConverter( mStageWidth, mStageHeight )
 			, mBlockViews()
+		{}
+
+		StageView* StageView::create( const int width, const int height, const OnBlockCallback& on_block_callback, const StageViewConfig config )
 		{
 			//
 			// Must odd number
 			//
-			CHECK_ODD_NUMBER( mStageWidth );
-			CHECK_ODD_NUMBER( mStageHeight );
-		}
+			CHECK_ODD_NUMBER( width );
+			CHECK_ODD_NUMBER( height );
 
-		StageView* StageView::create( const int width, const int height, const OnBlockCallback& on_block_callback, const StageViewConfig config )
-		{
 			auto ret = new ( std::nothrow ) StageView( width, height );
 			if( !ret || !ret->init( config, on_block_callback ) )
 			{
@@ -67,8 +67,9 @@ namespace step_clickclick
 			);
 			const Vec2 pivot_position( StageSize.width * -0.5f, StageSize.height * -0.5f );
 
-			setContentSize( StageSize );
-
+			//
+			// Pivot
+			//
 			if( config.bShowPivot )
 			{
 				auto pivot = Sprite::createWithSpriteFrameName( "helper_pivot.png" );
@@ -76,31 +77,38 @@ namespace step_clickclick
 				addChild( pivot, std::numeric_limits<int>::max() );
 			}
 
+			//
+			// Background
+			//
 			if( config.bShowBackgroundGuide )
 			{
-				auto pivot = LayerColor::create( Color4B( 0u, 0u, 0u, 100u ), getContentSize().width, getContentSize().height );
+				auto pivot = LayerColor::create( Color4B( 0u, 0u, 0u, 100u ), StageSize.width, StageSize.height );
 				pivot->setPosition( pivot_position );
 				addChild( pivot, std::numeric_limits<int>::min() );
 			}
 
-			// Buttons
-			int linear_index = 0;
-			BlockViewNode* block_view = nullptr;
-			for( int ty = 0; ty < mStageHeight; ++ty )
+			//
+			// Build Block View Node List
+			//
 			{
-				for( int tx = 0; tx < mStageWidth; ++tx )
+				int linear_index = 0;
+				BlockViewNode* block_view = nullptr;
+				for( int ty = 0; ty < mStageHeight; ++ty )
 				{
-					linear_index = mGridIndexConverter.To_Linear( tx, ty );
+					for( int tx = 0; tx < mStageWidth; ++tx )
+					{
+						linear_index = mGridIndexConverter.To_Linear( tx, ty );
 
-					block_view = BlockViewNode::create( linear_index, BlockSize, on_block_callback );
-					block_view->setPosition(
-						pivot_position
-						+ Vec2( BlockSize.width * 0.5f, BlockSize.height * 0.5f )
-						+ Vec2( tx * ( BlockSize.width + BlockMarginSize.width ), ty * ( BlockSize.height + BlockMarginSize.height ) )
-					);
-					addChild( block_view );
+						block_view = BlockViewNode::create( linear_index, BlockSize, on_block_callback );
+						block_view->setPosition(
+							pivot_position
+							+ Vec2( BlockSize.width * 0.5f, BlockSize.height * 0.5f )
+							+ Vec2( tx * ( BlockSize.width + BlockMarginSize.width ), ty * ( BlockSize.height + BlockMarginSize.height ) )
+						);
+						addChild( block_view );
 
-					mBlockViews.emplace_back( block_view );
+						mBlockViews.emplace_back( block_view );
+					}
 				}
 			}
 
@@ -132,5 +140,5 @@ namespace step_clickclick
 		{
 			mBlockViews[linear_index]->UpdateLife( last_life, current_life );
 		}
-	} // namespace game
-} // namespace step_clickclick
+	}
+}
