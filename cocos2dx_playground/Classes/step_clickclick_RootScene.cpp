495f9e9ee6a136f1bcba551fef5310742a156c10
r2road <studior2road@gmail.com>
2021-01-04 07:40:49 +0900

    ...

--- cocos2dx_playground/Classes/step_clickclick_RootScene.cpp
+++ cocos2dx_playground/Classes/step_clickclick_RootScene.cpp
@@ -90,7 +90,7 @@ namespace step_clickclick
 			ss << std::endl;
 			ss << "[SPACE] : " << step_clickclick::game::TitleScene::getTitle();
 
-			auto label = Label::createWithTTF( ss.str(), cpg::StringTable::GetFontPath(), 12, Size::ZERO, TextHAlignment::LEFT );
+			auto label = Label::createWithTTF( ss.str(), cpg::StringTable::GetFontPath(), 10, Size::ZERO, TextHAlignment::LEFT );
 			label->setAnchorPoint( Vec2( 0.5f, 0.5f ) );
 			label->setPosition( Vec2(
 				visibleOrigin.x + ( visibleSize.width * 0.5f )
