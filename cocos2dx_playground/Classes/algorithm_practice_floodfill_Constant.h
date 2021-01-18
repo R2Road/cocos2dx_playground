a6bf09a477048943458fa8e0a30575aa5e309599
r2road <studior2road@gmail.com>
2021-01-19 00:02:46 +0900

    add class : GridDirection

--- cocos2dx_playground/Classes/algorithm_practice_floodfill_Constant.h
+++ cocos2dx_playground/Classes/algorithm_practice_floodfill_Constant.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "cpg_Point.h"
+
 namespace algorithm_practice_floodfill
 {
 	enum eToolIndex
@@ -15,8 +17,31 @@ namespace algorithm_practice_floodfill
 		Wall,
 	};
 
+	enum eDirectionType
+	{
+		Up = 1,
+		Right = 1 << 1,
+		Down = 1 << 2,
+		Left = 1 << 3,
+
+		FIRST = Up,
+	};
+
+	class GridDirection
+	{
+	public:
+		GridDirection();
+
+		bool HasDirection() const { return 0 != mCurrentDirection; }
+		cpg::Point PopNextDirection();
+
+	private:
+		char mCurrentDirection;
+	};
+
 	struct GridValue
 	{
 		eGridType Type = eGridType::Road;
+		GridDirection Direction;
 	};
 }
