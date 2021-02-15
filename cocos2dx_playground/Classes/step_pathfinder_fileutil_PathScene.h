d41de8f750df3151493d48bc254dc4ba0c7ca558
r2road <studior2road@gmail.com>
2021-02-16 07:39:11 +0900

    private

--- cocos2dx_playground/Classes/step_pathfinder_fileutil_PathScene.h
+++ cocos2dx_playground/Classes/step_pathfinder_fileutil_PathScene.h
@@ -16,7 +16,10 @@ namespace step_pathfinder
 			static const char* getTitle() { return "FileUtil : Path"; }
 			static cocos2d::Scene* create( const helper::FuncSceneMover& back_to_the_previous_scene_callback );
 
+		private:
 			bool init() override;
+
+		public:
 			void onEnter() override;
 			void onExit() override;
 
