3289faacc2615e5b921640e7ec6870add07ed649
r2road <studior2road@gmail.com>
2021-03-19 00:54:54 +0900

    ...

--- cocos2dx_playground/Classes/cpg_input_KeyCodeContainer.h
+++ cocos2dx_playground/Classes/cpg_input_KeyCodeContainer.h
@@ -2,11 +2,12 @@
 
 #include <bitset>
 
-#include "cocos/base/CCEventKeyboard.h"
+#include "base/CCEventKeyboard.h"
 
 namespace cpg_input
 {
 	const std::size_t KeyCodeContainerFirst = static_cast<std::size_t>( cocos2d::EventKeyboard::KeyCode::KEY_NONE );
 	const std::size_t KeyCodeContainerSize = static_cast<std::size_t>( static_cast<int>( cocos2d::EventKeyboard::KeyCode::KEY_PLAY ) + 1 );
+
 	using KeyCodeContainerT = std::bitset<KeyCodeContainerSize>;
 }
