d27d421bde525728a9d91d09ac8349507c816d24
r2road <studior2road@gmail.com>
2021-10-06 17:52:33 +0900

    not need

--- cocos2dx_playground/Classes/cpg_input_iKeyCollector.cpp
+++ cocos2dx_playground/Classes/cpg_input_iKeyCollector.cpp
@@ -1,7 +1,5 @@
 #include "cpg_input_iKeyCollector.h"
 
-USING_NS_CC;
-
 namespace cpg_input
 {
 	iKeyCollector::iKeyCollector( const KeyMapSp& _key_map_container ) : mKeyMapContainer( _key_map_container ) {}
