03d04e50247d4ee8d31a8f3a2b83420d27e444cb
r2road <studior2road@gmail.com>
2021-12-30 14:17:31 +0900

    remove cpp

--- cocos2dx_playground/Classes/algorithm_practice_floodfill_Constant.h
+++ cocos2dx_playground/Classes/algorithm_practice_floodfill_Constant.h
@@ -8,22 +8,77 @@ namespace algorithm_practice_floodfill
 	class Cell4FloodFill
 	{
 	public:
-		Cell4FloodFill();
+		Cell4FloodFill() :
+			mValid( false )
+			, mParentPoint( { -1, -1 } )
+			, mTotalDirection( cpg::Direction4::eState::None )
+			, mCurrentDirection( cpg::Direction4::eState::FIRST )
+		{}
 
-		void Clear() { mValid = false; mParentPoint = { -1, -1 }; mTotalDirection = cpg::Direction4::eState::None; };
-		void Begin( const cpg::Point parent_point, const cpg::Direction4 parent_direction );
-
-		bool IsValid() const { return mValid; }
-
+		//
+		// Getter
+		//
 		cpg::Point GetParentPoint() const { return mParentPoint; }
-
 		char GetTotalDirection() const { return mTotalDirection; }
 		cpg::Direction4 GetCurrentDirection() const { return mCurrentDirection.GetState(); }
 
-		bool HasDirection() const { return 0 != mTotalDirection; }
-		cpg::Direction4 PopDirection();
+		bool IsValid() const { return mValid; }
 
-		void RotateCurrentDirection( const bool rotate_right );
+		void Clear()
+		{
+			mValid = false;
+			mParentPoint = { -1, -1 };
+			mTotalDirection = cpg::Direction4::eState::None;
+		};
+		void Begin( const cpg::Point parent_point, const cpg::Direction4 parent_direction )
+		{
+			mValid = true;
+
+			mParentPoint = parent_point;
+
+			if( cpg::Direction4::eState::None == parent_direction.GetState() )
+			{
+				mCurrentDirection = cpg::Direction4::eState::FIRST;
+			}
+			else
+			{
+				mCurrentDirection = parent_direction;
+			}
+
+			mTotalDirection = cpg::Direction4::eState::ALL;
+
+			auto temp_direction4 = parent_direction;
+			temp_direction4.Rotate( true );
+			temp_direction4.Rotate( true );
+			mTotalDirection ^= temp_direction4.GetState();
+		}
+		
+		bool HasDirection() const
+		{
+			return 0 != mTotalDirection;
+		}
+		cpg::Direction4 PopDirection()
+		{
+			cpg::Direction4 out_direction( cpg::Direction4::None );
+
+			if( HasDirection() )
+			{
+				while( 0 == ( mTotalDirection & mCurrentDirection.GetState() ) )
+				{
+					mCurrentDirection.Rotate( true );
+				}
+
+				mTotalDirection ^= mCurrentDirection.GetState();
+
+				out_direction = mCurrentDirection;
+			}
+
+			return out_direction;
+		}
+		void RotateCurrentDirection( const bool rotate_right )
+		{
+			mCurrentDirection.Rotate( rotate_right );
+		}
 
 	private:
 		bool mValid;
